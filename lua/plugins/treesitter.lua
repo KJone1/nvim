04eff4d79c83d0ee836a53b83f62911e9acdc7cb
KJone1 <davidyahin@protonmail.com>
2023-12-28 23:59:20 +0200

    Config treesitterr

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,4 +5,14 @@ return  {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   build = ':TSUpdate',
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+      sync_install = false,
+      auto_install = false,
+      ignore_install = {},
+      modules = {}
+    }
+  end
 }
