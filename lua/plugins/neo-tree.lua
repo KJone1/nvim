5086428c04c4e43c39da8d87724bd1116fe5c77b
KJone1 <davidyahin@protonmail.com>
2024-02-03 16:17:22 +0200

    Remove startup logic in favor of spaceport

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -36,12 +36,10 @@ return {
       },
   })
     local args = vim.fn.argv()
-    if not vim.g.neotree_opened and #args == 0 then
-      vim.cmd "Neotree filesystem reveal action=focus position=current"
-      vim.g.neotree_opened = true
-    end
-      vim.keymap.set('n', 'po', ':Neotree filesystem action=show toggle=true<CR>', { noremap = true, silent = true, desc = '[O]pen File Tree' })
-      vim.keymap.set('n', 'pf', ':Neotree filesystem action=focus<CR>', { noremap = true, silent = true, desc = '[F]ocs on File Tree' })
+    -- if not vim.g.neotree_opened and #args == 0 then
+    --   vim.cmd "Neotree filesystem reveal action=focus position=current"
+    --   vim.g.neotree_opened = true
+    -- end
       vim.keymap.set('n', 'pot', ':Neotree filesystem action=show toggle=true<CR>', { noremap = true, silent = true, desc = '[T]oggle File Tree' })
       vim.keymap.set('n', 'pof', ':Neotree filesystem action=focus<CR>', { noremap = true, silent = true, desc = '[F]ocus on File Tree' })
   end
