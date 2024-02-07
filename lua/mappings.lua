402556df2aaefb2b3a73b931f38af519b1897de8
KJone1 <davidyahin@protonmail.com>
2024-02-07 21:19:49 +0200

    Moved markdown-preview mapping to mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,8 +2,8 @@ local M = {}
 
 M.general = {
   n = { -- Normal mode mappings
-      ["pt"] = {':Twilight<CR>','Twilight Mode' },
-      ["pp"] = {':Spaceport<CR>','Open Projects' }
+      ['pt'] = {':Twilight<CR>','Twilight Mode' },
+      ['pp'] = {':Spaceport<CR>','Open Projects' }
   },
   i = { -- Insert mode mappings
   },
@@ -13,15 +13,21 @@ M.general = {
 
 M.git = {
   n = {
-    ["lg"] = {":LazyGit<CR>", "Lazy Git" },
+    ['lg'] = {':LazyGit<CR>', 'Lazy Git' },
   },
 }
 
 M.tree = {
   n = {
-    ["pot"] = {':Neotree filesystem action=show toggle=true<CR>','Toggle File Tree' },
-    ["pof"] = {':Neotree filesystem action=focus<CR>','Focus on File Tree' },
+    ['pot'] = {':Neotree filesystem action=show toggle=true<CR>','Toggle File Tree' },
+    ['pof'] = {':Neotree filesystem action=focus<CR>','Focus on File Tree' },
   },
+}
+
+M.text = {
+  n = {
+    ['pm'] = {':MarkdownPreview<CR>','Open Markdown Preview' }
+  }
 }
 
 return M
--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,5 +3,4 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-    vim.keymap.set('n', 'pm', ':MarkdownPreview<CR>', { noremap = true, silent = true, desc = 'Open [M]arkdown Preview' })
 }
