62d2350f8560cca5b7ae91b958bf36f449ff2565
KJone1 <davidyahin@protonmail.com>
2024-02-09 00:11:55 +0200

    Formatting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,27 +2,27 @@ local M = {}
 
 M.general = {
   n = { -- Normal mode mappings
-      ['pt'] = {':Twilight<CR>','Twilight Mode' },
-      ['pp'] = {':Spaceport<CR>','Open Projects' },
-      ['<leader>/'] = {'<Cmd>lua require("Comment.api").toggle.linewise.current()<CR>', 'Comment linewise' }
+    ['pt'] = {':Twilight<CR>','Twilight Mode' },
+    ['pp'] = {':Spaceport<CR>','Open Projects' },
+    ['<leader>/'] = {'<Cmd>lua require("Comment.api").toggle.linewise.current()<CR>', 'Comment linewise' }
   },
   i = { -- Insert mode mappings
   },
   v = { -- Visual mode mappings
-      ['<leader>/'] = {'<ESC><Cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', 'Comment linewise selected text' }
+    ['<leader>/'] = {'<ESC><Cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', 'Comment linewise selected text' }
   },
 }
 
 M.git = {
   n = {
-      ['lg'] = {':LazyGit<CR>', 'Lazy Git' },
+    ['lg'] = {':LazyGit<CR>', 'Lazy Git' },
   },
 }
 
 M.tree = {
   n = {
-      ['pot'] = {':Neotree filesystem action=show toggle=true<CR>','Toggle File Tree' },
-      ['pof'] = {':Neotree filesystem action=focus<CR>','Focus on File Tree' },
+    ['pot'] = {':Neotree filesystem action=show toggle=true<CR>','Toggle File Tree' },
+    ['pof'] = {':Neotree filesystem action=focus<CR>','Focus on File Tree' },
   },
   v = {
   }
