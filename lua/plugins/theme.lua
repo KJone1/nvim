59f654b9878ead9674cb763a9ed03e5a7575905e
KJone1 <davidyahin@protonmail.com>
2024-02-09 13:34:17 +0200

    Add new themes and update default theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,28 @@
-return{
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'gruvbox-material'
-  end
+local themes = {
+  {
+    "sainnhe/gruvbox-material",
+    lazy = true,
+    event = "User ColorSchemeLoad",
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    event = "User ColorSchemeLoad",
+    priority = 1000,
+    config = function()
+      -- Default theme
+      vim.cmd.colorscheme("kanagawa-wave")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    lazy = true,
+    event = "User ColorSchemeLoad",
+    priority = 1000,
+  },
 }
+
+return themes
+
