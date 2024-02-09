8c2af62e6f109d7c1766e307597a6649499af2c7
KJone1 <davidyahin@protonmail.com>
2024-02-09 21:16:07 +0200

    Formatting

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,7 +2,5 @@ return {
   "okuuva/auto-save.nvim",
   cmd = "ASToggle", -- optional for lazy loading on command
   event = { "InsertLeave" }, -- optional for lazy loading on trigger events
-  opts = {
-  -- your config goes here
-  },
+  opts = {},
 }
