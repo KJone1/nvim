f72ae704f36938bf82e733ca3f48012392079164
KJone1 <davidyahin@protonmail.com>
2024-02-10 13:16:22 +0200

    Remove comments

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,9 +1,6 @@
--- nvim v0.8.0
 return{
     "kdheepak/lazygit.nvim",
-        -- optional for floating window border decoration
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    -- vim.keymap.set('n', 'pg', ':LazyGit<CR>', { noremap = true, silent = true, desc = 'Open Lazy[G]it' })
 }
