bb4e08aecefe65311358729686e2afc5cdda461e
KJone1 <davidyahin@protonmail.com>
2024-02-12 22:04:24 +0200

    Update auto-save events

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,6 +1,6 @@
 return {
   "okuuva/auto-save.nvim",
   cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "InsertLeave" }, -- optional for lazy loading on trigger events
+  event = { "InsertLeave", "BufLeave", "FocusLost" }, -- optional for lazy loading on trigger events 
   opts = {},
 }
