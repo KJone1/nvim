45f9c6ffff6c283a45ca454e2ce4311816f135ae
KJone1 <davidyahin@protonmail.com>
2024-02-16 00:12:21 +0200

    Update themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,21 +6,27 @@ local themes = {
     priority = 1000,
   },
   {
-    -- Default theme
     "rebelot/kanagawa.nvim",
+    lazy = true,
+    event = "User ColorSchemeLoad",
+    priority = 1000,
+  },
+  {
+    "getomni/neovim",
+    as = "omni",
+    lazy = true,
+    event = "User ColorSchemeLoad",
+    priority = 1000,
+  },
+  {
+    -- Default theme
+    "Yazeed1s/oh-lucy.nvim",
     lazy = false,
     event = "User ColorSchemeLoad",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("kanagawa-wave")
+      vim.cmd.colorscheme("oh-lucy-evening")
     end,
-  },
-  {
-    "catppuccin/nvim",
-    as = "catppuccin",
-    lazy = true,
-    event = "User ColorSchemeLoad",
-    priority = 1000,
   },
 }
 
